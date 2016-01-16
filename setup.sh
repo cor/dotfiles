429d4abc136c65e404a14fba0ef53f557b249afe
cor <cor@pruijs.nl>
2016-01-16 22:30:54 +0100

    Add .tmux.conf

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ ln -s ~/Setup/dotfiles/nvim/autoload/plug.vim ~/.config/nvim/autoload/plug.vim
 rm ~/.config/nvim/init.vim
 ln -s ~/Setup/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 
+rm ~/.tmux.conf
+ln -s ~/Setup/dotfiles/.tmux.conf ~/.tmux.conf
+
