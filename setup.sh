46d2f3652d15bd23fb892f270a4ed6349705299e
cor <cor@pruijs.nl>
2016-05-25 21:49:15 +0200

    Add sublime-keymap support

--- setup.sh
+++ setup.sh
@@ -1,6 +1,20 @@
 #! /bin/bash
 
 # Create required directories
+
+# Sublime Text
+rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln  ~/Setup/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+
+rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
+ln  ~/Setup/dotfiles/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
+
+# ZSH
+rm ~/.zshrc
+ln -s ~/Setup/dotfiles/.zshrc ~/.zshrc
+
+
+# Neovim
 cd ~
 mkdir .config
 cd .config
@@ -9,18 +23,14 @@ cd nvim
 mkdir autoload
 cd ~
 
-rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-ln  ~/Setup/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-
-rm ~/.zshrc
-ln -s ~/Setup/dotfiles/.zshrc ~/.zshrc
-
 rm ~/.config/nvim/autoload/plug.vim
 ln -s ~/Setup/dotfiles/nvim/autoload/plug.vim ~/.config/nvim/autoload/plug.vim
 
 rm ~/.config/nvim/init.vim
 ln -s ~/Setup/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 
+
+# Tmux
 rm ~/.tmux.conf
 ln -s ~/Setup/dotfiles/.tmux.conf ~/.tmux.conf
 
