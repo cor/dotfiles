3052d6588a9d3ff0a27edbabcb63f835f3f617fc
cor <cor@pruijs.nl>
2018-03-17 16:25:26 +0100

    Add .ideavimrc

--- setup.sh
+++ setup.sh
@@ -7,5 +7,9 @@ ln -s ~/Setup/dotfiles/zsh/.zshrc ~/.zshrc
 # Neovim
 mkdir -p ~/.config/nvim/
 ln -s ~/Setup/dotfiles/nvim/init.vim ~/.config/nvim/init.vim 
+
+# Idea
+ln -s ~/Setup/dotfiles/idea/.ideavimrc ~/.ideavimrc
+
 # hush macoS login
 touch ~/.hushlogin
