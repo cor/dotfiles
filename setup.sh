2178d32cce19d674b4ab5a941220f2ded45915a7
cor <cor@pruijs.nl>
2020-10-09 23:40:36 +0200

    Comment unused configs

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
-
-# ZSH
-rm ~/.zshrc
-ln -s ~/Setup/dotfiles/zsh/.zshrc ~/.zshrc
-
+# 
+# # ZSH
+# rm ~/.zshrc
+# ln -s ~/Setup/dotfiles/zsh/.zshrc ~/.zshrc
+# 
 # Neovim
 mkdir -p ~/.config/nvim/
 ln -s ~/Setup/dotfiles/nvim/init.vim ~/.config/nvim/init.vim 
-
-# Idea
-ln -s ~/Setup/dotfiles/idea/.ideavimrc ~/.ideavimrc
-
+#
+# # Idea
+# ln -s ~/Setup/dotfiles/idea/.ideavimrc ~/.ideavimrc
+#
 # hush macoS login
 touch ~/.hushlogin
